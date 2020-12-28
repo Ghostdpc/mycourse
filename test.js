f12946a6101b8caebf7cd00bcbc14931e89f2de5
Ghostdpc <duanpengchen@163.com>
2020-12-28 23:10:17 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,20 @@
 function a(na) {
-    if(na) {
+    if (na) {
         this.name = na;
     }
     console.log(this.name);
 }
 
 a.prototype.name = "aa";
-
 a();
 a("bb");
 var i = new a();
 var c = new a("ee");
 
-
 var j = {
-    c:1,
-    p:a
-}
+    c: 1,
+    p: a,
+};
 
 j.p();
 j.p("dd");
