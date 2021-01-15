447924a3be8618157dcdfbd6135f61f90c25c1fe
Ghostdpc <duanpengchen@163.com>
2021-01-15 18:38:13 +0800

    homework page

--- lesson 3-4/my-gridsome-site/gridsome.config.js
+++ lesson 3-4/my-gridsome-site/gridsome.config.js
@@ -1,10 +1,41 @@
-// This is where project configuration and plugin options are located.
+// This is where project configuration and installed plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Portfolio",
+  siteUrl: `https://www.itsnwa.com`,
+  host: "0.0.0.0",
+  titleTemplate: "%s - NWA",
+  siteDescription: "Creative technologist",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "projects/**/*.md",
+        typeName: "ProjectPost",
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    },
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "journal/**/*.md",
+        typeName: "JournalPost",
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    }
+  ],
+  transformers: {
+    remark: {
+      plugins: ["@gridsome/remark-prismjs"]
+    }
+  }
+};
